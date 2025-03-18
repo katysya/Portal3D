1d8b7942f44176b29c5ec7b1c4c688e00204d7bd
katyusya <katusha.fedyakena@mail.ru>
2025-03-18 23:41:53 +0300

    Changed render components of models.

--- src/features/Section/ui/SectionPortal/SectionPortal.jsx
+++ src/features/Section/ui/SectionPortal/SectionPortal.jsx
@@ -24,22 +24,60 @@ const SectionPortal = () => {
       controlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true,);
     }
 
-  }, [active, scene]); 
+  }, [active, scene]);
+  
+  const modelsData = [
+    {
+      name: "Fish King",
+      texture: "textures/anime_art_style_a_water_based_pokemon.jpg",
+      color: "#000a52",
+      ModelComponent: Fish,
+      position: [-0, 0, 0],
+      rotation: [0, 0, 0],
+    },
+    {
+      name: "Bunny",
+      texture: "textures/anime_art_style_cactus_forest.jpg",
+      color: "#2d2d2d",
+      ModelComponent: Bunny,
+      position: [-2.5, 0, 0],
+      rotation: [0, Math.PI / 8, 0],
+    },
+    {
+      name: "Frog",
+      texture: "textures/anime_art_style_lava_world.jpg",
+      color: "#915e00",
+      ModelComponent: Frog,
+      position: [2.5, 0, 0],
+      rotation: [0, -Math.PI / 6, 0],
+    },
+  ];
 
   return (
     <>
     <ambientLight intensity={0.5}/>
     <Environment preset="sunset"/>
       <CameraControls ref={controlsRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 6}/>
-      <ModelCard texture={'textures/anime_art_style_a_water_based_pokemon.jpg'} name={'Fish King'} color={'#000a52'} active={active} setActive={setActive} hovered={hovered} setHovered={setHovered}>
-      <Fish scale={0.6} position-y={-1} hovered={hovered === "Fish King"}/>
+      {modelsData.map(({ name, texture, color, ModelComponent, position, rotation }) => (
+      <ModelCard
+        key={name}
+        texture={texture}
+        name={name}
+        color={color}
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+        position-x={position[0]}
+        position-y={position[1]}
+        position-z={position[2]}
+        rotation-x={rotation[0]}
+        rotation-y={rotation[1]}
+        rotation-z={rotation[2]}
+      >
+        {ModelComponent && <ModelComponent scale={0.6} position-y={-1} hovered={hovered === name} />}
       </ModelCard>
-      <ModelCard texture={'textures/anime_art_style_cactus_forest.jpg'} name={'Bunny'} color={'#2d2d2d'} active={active} setActive={setActive} hovered={hovered} setHovered={setHovered} position-x={-2.5} rotation-y={Math.PI / 8}>
-      <Bunny scale={0.6} position-y={-1} hovered={hovered === "Bunny"}/>
-      </ModelCard>
-      <ModelCard texture={'textures/anime_art_style_lava_world.jpg'} name={'Frog'} color={'#915e00'} active={active} setActive={setActive} hovered={hovered} setHovered={setHovered} position-x={2.5} rotation-y={-Math.PI / 6}>
-      <Frog scale={0.6} position-y={-1} hovered={hovered === "Frog"}/>
-      </ModelCard>
+    ))}
     </>
   );
 };
